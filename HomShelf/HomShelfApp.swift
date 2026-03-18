aacaeef768c294cbdeefb43f5853645889cf9600
maniraj-mm48 <maniraj.mm48@gmail.com>
2026-03-17 22:32:46 -0400

    Initial Commit

---
+++ HomShelf/HomShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomShelfApp.swift
+//  HomShelf
+//
+//  Created by Maniraj on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct HomShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
